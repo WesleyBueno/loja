9e5ba1130c813ede20d8b2865dff925ec6c04c8b
Wesley Silva <wesley.bueno.silva.wbs@gmail.com>
2020-12-05 10:30:48 -0300

    esqueleto angular

---
+++ reVestir/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { TrocaComponent } from './troca/troca.component';
+import { LojasComponent } from './lojas/lojas.component';
+import { ClubeComponent } from './clube/clube.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ShopComponent,
+    TrocaComponent,
+    LojasComponent,
+    ClubeComponent,
+    CarrinhoComponent,
+    CadastrarComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
