6616cf769c42f81ce9370dd1b03790c7914b0c58
Wesley Silva <wesley.bueno.silva.wbs@gmail.com>
2020-12-05 20:54:47 -0300

    Estrutura completa

--- reVestir/src/app/app-routing.module.ts
+++ reVestir/src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { ClubeComponent } from './clube/clube.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { LojasComponent } from './lojas/lojas.component';
 import { AuthGuard } from './service/auth.guard';
 import { ShopComponent } from './shop/shop.component';
+import { TrocaComponent } from './troca/troca.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'shop', component: ShopComponent, canActivate: [AuthGuard]},
+  {path: 'shop', component: ShopComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'cadastrar', component: CadastrarComponent}
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'carrinho', component: CarrinhoComponent},
+  {path: 'clube', component: ClubeComponent},
+  {path: 'lojas', component: LojasComponent},
+  {path: 'troca', component: TrocaComponent}
 ];
 
 @NgModule({
